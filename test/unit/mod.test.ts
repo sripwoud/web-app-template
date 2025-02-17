6f37fbd23105410f2f5da18bc8437fd7ae621a70
sripwoud <me@sripwoud.xyz>
2025-02-17 13:23:50 +0100

    feat: scaffold vite

--- test/unit/mod.test.ts
+++
@@ -1,8 +0,0 @@
-import { TEST } from '@mod/file'
-import { describe, expect, it } from 'bun:test'
-
-describe('Test', () => {
-  it('some test', () => {
-    expect(TEST).toEqual('abc')
-  })
-})
---
+++ web/src/App.tsx
@@ -0,0 +1,11 @@
+import './App.css'
+
+function App() {
+  return (
+    <>
+      Hello Vite + React + TS
+    </>
+  )
+}
+
+export default App
